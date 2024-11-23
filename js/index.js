5dd92fb187ac5b052727f166f8d5e1860d83e1ea
GuySeguin <91144923+GuySeguin@users.noreply.github.com>
2024-11-22 20:13:32 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector("#home").style.display = "none";
         document.querySelector("#browse").style.display = "block";
         const url = "https://www.randyconnolly.com/funwebdev/3rd/api/fi/races.php";
-       fetch(url+"?seasons="+select.value).then(response => {if (response.ok) {return response.json()} else {throw new error("AAAAAAHHH!")}}).then(
+       fetch(url+"?season="+select.value).then(response => {if (response.ok) {return response.json()} else {throw new error("AAAAAAHHH!")}}).then(
         data => {browseView(data)}
        ).catch( error => {"AAAAAAAHHH!"});
     })
