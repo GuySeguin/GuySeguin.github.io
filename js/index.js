2dd3444350f42048de567e7af0c7b12ef5407837
GuySeguin <91144923+GuySeguin@users.noreply.github.com>
2024-11-22 22:29:32 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     function browseView(data) {
         console.log(data);
-        //raceTable(data);
+        raceTable(data);
     }
 
-    function raceTable(testData) {
+    function raceTable(data) {
         const row1 = document.querySelector("#raceTable");
         const tr = document.createElement("tr");
         const th1 = document.createElement("th");
@@ -21,27 +21,49 @@ document.addEventListener('DOMContentLoaded', function () {
         const th2 = document.createElement("th");
         th2.textContent = "Race Name";
     const th3 = document.createElement("th");
-        th3.textContent = "Circuit ID";
+        th3.textContent = "Circuit";
+        const th4 = document.createElement("th");
+        th4.textContent = "Date";
+        const th5 = document.createElement("th");
+        th5.textContent = "Round";
+    const th6 = document.createElement("th");
+        th6.textContent = "URL";
         tr.appendChild(th1);
         tr.appendChild(th2);
         tr.appendChild(th3);
+        tr.appendChild(th4);
+        tr.appendChild(th5);
+        tr.appendChild(th6);
         row1.appendChild(tr);
 
-        for (let r of testData) {
+        for (let r of data) {
             const nuTr = document.createElement("tr");
             const td1 = document.createElement("td");
-        td1.textContent = r.raceId;
+        td1.textContent = r.id;
         const td2 = document.createElement("td");
         td2.textContent = r.name;
     const td3 = document.createElement("td");
-        td3.textContent = r.circuit;
+        td3.textContent = r.circuit.name;
+        const td4 = document.createElement("td");
+        td4.textContent = r.date;
+        const td5 = document.createElement("td");
+        td5.textContent = r.round;
+    const td6 = document.createElement("td");
+    const a = document.createElement("a");
+    a.setAttribute("href", r.url);
+    a.textContent = "Website";
+
+        td6.appendChild(a);
         nuTr.appendChild(td1);
         nuTr.appendChild(td2);
         nuTr.appendChild(td3);
-        const td4 = document.createElement("button");
-        td4.setAttribute("id", r.raceId);
-        td4.textContent = "Results";
         nuTr.appendChild(td4);
+        nuTr.appendChild(td5);
+        nuTr.appendChild(td6);
+        const td7 = document.createElement("button");
+        td7.setAttribute("id", r.id);
+        td7.textContent = "Results";
+        nuTr.appendChild(td7);
         row1.appendChild(nuTr);
         }
         row1.addEventListener("click", (e)=> {
