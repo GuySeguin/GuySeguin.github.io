8f1b23bcebdadd577c3f9e5bb674847a697d0bad
GuySeguin <91144923+GuySeguin@users.noreply.github.com>
2024-12-04 23:46:43 -0700

    Qualifying and Race tables stuff

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () { 
 
 let selectSeason;
-    
+let resultData;
+let qualifyData;
+//Promise Method for fetches
     function getSeasonData() {
         let raceURL = "https://www.randyconnolly.com/funwebdev/3rd/api/f1/races.php?season="
         let resultURL = "https://www.randyconnolly.com/funwebdev/3rd/api/f1/results.php?season="
         let qualifyURL = "https://www.randyconnolly.com/funwebdev/3rd/api/f1/qualifying.php?season="
        
-        let prom1 = fetch(raceURL+selectSeason).then(r => r.json())
-        let prom2 = fetch(resultURL+selectSeason).then(r => r.json())
-        let prom3 = fetch(qualifyURL+selectSeason).then(r => r.json())
+        let prom1 = fetch(raceURL+selectSeason).then(response => {if (response.ok) {return response.json()} else {throw new error("AAAAAAHHH!")}})
+        let prom2 = fetch(resultURL+selectSeason).then(response => {if (response.ok) {return response.json()} else {throw new error("AAAAAAHHH!")}})
+        let prom3 = fetch(qualifyURL+selectSeason).then(response => {if (response.ok) {return response.json()} else {throw new error("AAAAAAHHH!")}})
         console.log(selectSeason);
         return Promise.all([prom1, prom2, prom3]);
 
     }
     const select = document.querySelector("#home select");
+    //Event listener for season select menu
     select.addEventListener("change", (e) => {
         selectSeason = e.target.value;
         document.querySelector("#home").style.display = "none";
         document.querySelector("#browse").style.display = "block";
         const url = "https://www.randyconnolly.com/funwebdev/3rd/api/f1/races.php";
-        let resultData;
-        let qualifyData;
+        
         let raceData = localStorage.getItem(`race${selectSeason}`);
         if (!raceData) {
             getSeasonData().then(data =>{
@@ -39,16 +41,19 @@ let selectSeason;
             });
         } else {
             resultData = JSON.parse(localStorage.getItem(`result${selectSeason}`));
-        resultData = JSON.parse(localStorage.getItem(`qualify${selectSeason}`));
+        qualifyData = JSON.parse(localStorage.getItem(`qualify${selectSeason}`));
+       
             browseView(JSON.parse(raceData));
         }
        
-       //fetch(url+"?season="+select.value).then(response => {if (response.ok) {return response.json()} else {throw new error("AAAAAAHHH!")}}).then(
-      //  data => {browseView(data)}
-   //    ).catch( error => {"AAAAAAAHHH!"});
-    })
+    //   fetch("https://randyconnolly.com/funwebdev/3rd/api/f1/drivers.php?id=1").then(response => {if (response.ok) {return response.json()} else {throw new error("AAAAAAHHH!")}}).then(
+      //  data => {console.log(data)}
+      // ).catch( error => {"AAAAAAAHHH!"});
+   })
     function browseView(data) {
-        console.log(data);
+        console.log(resultData); 
+        console.log(qualifyData);
+
         data = sortByRound(data);
 
         raceTable(data);
@@ -96,40 +101,55 @@ let selectSeason;
     function listeners(data) {
     document.querySelector("#raceTable").addEventListener("click", (e)=> {
         if (e.target.nodeName=="BUTTON") {
-            resultsTable(e.target.id);
-        } else if (e.target.nodeName=="TH") {
+            document.querySelector("#resultTable tbody").innerHTML = null;
+            document.querySelector("#raceResults").style.display = "block";
+            document.querySelector("#qualifyingTable tbody").innerHTML = null;
+            const resultsInfo = resultData.filter(r => {
+                if (r.race.id == e.target.id) {
+                    return r;
+                }
+            })
+            const qualifyInfo = qualifyData.filter(q => {
+                if (q.race.id == e.target.id) {
+                    return q;
+                }
+            })
             
+            resultsTable(resultsInfo);
+           qualifyingTable(qualifyInfo);
+        } else if (e.target.nodeName=="TH") {
+            wipeClasses();
             if (e.target.id=="raceid") {
                
-               document.querySelector("tbody").innerHTML = null;
+               document.querySelector("#raceTable tbody").innerHTML = null;
                 sortByID(data);
                 raceTable(data);
               document.querySelector("#raceid").classList.toggle("sort");
 
             } else if (e.target.id == "rName") {
                 
-                document.querySelector("tbody").innerHTML = null;
+                document.querySelector("#raceTable tbody").innerHTML = null;
                 sortByName(data);
                 raceTable(data);
                 document.querySelector("#rName").classList.toggle("sort");
 
             } else if (e.target.id == "rCircuit") {
                 
-                document.querySelector("tbody").innerHTML = null;
+                document.querySelector("#raceTable tbody").innerHTML = null;
                 sortByCircuit(data);
                 raceTable(data);
                 document.querySelector("#rCircuit").classList.toggle("sort");
 
             } else if (e.target.id == "rDate") {
                 
-                document.querySelector("tbody").innerHTML = null;
+                document.querySelector("#raceTable tbody").innerHTML = null;
                 sortByDate(data);
                 raceTable(data);
                 document.querySelector("#rDate").classList.toggle("sort");
 
             } else if (e.target.id == "rRound") {
                 
-                document.querySelector("tbody").innerHTML = null;
+                document.querySelector("#raceTable tbody").innerHTML = null;
                 sortByRound(data);
                 raceTable(data);
                 document.querySelector("#rRound").classList.toggle("sort");
@@ -139,6 +159,80 @@ let selectSeason;
 
         }
     })}
+    function resultsTable(raceResults) {
+        const row1 = document.querySelector("#resultTable tbody");
+       
+        for (let r of raceResults) {
+
+            const nuTr = document.createElement("tr");
+            nuTr.classList.add("raceRow");
+
+            const td1 = document.createElement("td");
+            td1.textContent = r.position;
+            const td2 = document.createElement("td");
+            td2.textContent = `${r.driver.forename} ${r.driver.surname}`;
+            td2.setAttribute("data-driver", r.driver.id);
+            td2.classList.add("driver");
+
+            const td3 = document.createElement("td");
+            td3.textContent = r.constructor.name;
+            const td4 = document.createElement("td");
+            td4.textContent = r.laps;
+            const td5 = document.createElement("td");
+            td5.textContent = r.points;
+   
+            nuTr.appendChild(td1);
+            nuTr.appendChild(td2);
+            nuTr.appendChild(td3);
+            nuTr.appendChild(td4);
+            nuTr.appendChild(td5);
+       
+            row1.appendChild(nuTr);
+        }
+    }
+    function qualifyingTable(raceyRaces) {
+        const row1 = document.querySelector("#qualifyingTable tbody");
+       
+        for (let r of raceyRaces) {
+
+            const nuTr = document.createElement("tr");
+            nuTr.classList.add("raceRow");
+
+            const td1 = document.createElement("td");
+            td1.textContent = r.position;
+            const td2 = document.createElement("td");
+            td2.textContent = `${r.driver.forename} ${r.driver.surname}`;
+            td2.setAttribute("data-driver", r.driver.id);
+            td2.classList.add("driver");
+
+            const td3 = document.createElement("td");
+            td3.textContent = r.constructor.name;
+            const td4 = document.createElement("td");
+            td4.textContent = r.laps;
+            const td5 = document.createElement("td");
+            td5.textContent = r.points;
+            const td6 = document.createElement("td");
+            td6.textContent = r.points;
+
+            nuTr.appendChild(td1);
+            nuTr.appendChild(td2);
+            nuTr.appendChild(td3);
+            nuTr.appendChild(td4);
+            nuTr.appendChild(td5);
+            nuTr.appendChild(td6);
+
+            row1.appendChild(nuTr);
+        }
+
+    }
+    function wipeClasses() {
+        const th = document.querySelectorAll("th");
+        th.forEach(t => {
+            
+                t.className = "";
+            
+        })
+    }
     function sortByName(data) {
         data.sort((a,b)=>{
             return a.name.localeCompare(b.name);
